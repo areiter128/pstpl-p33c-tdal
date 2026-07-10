0a04601e336c659002431687d8f7e80b5e0b9d57
M91406 <andreas.reiter@microchip.com>
2026-07-10 11:48:04 +0700

    Fixed issues with SPI instance declaration and no-pin dummy-function declarations

--- p33c_pral/exth/p33c_pinmap.h
+++ p33c_pral/exth/p33c_pinmap.h
@@ -40,6 +40,26 @@
 #include "p33c_devices.h" // include SMPS device family abstraction declarations
 #include "p33c_gpio.h" // include generic GPIO data type declaration header file
 
+// Dummy functions for PIN_NONE objects (not accessing hardware)
+static inline bool __attribute__ ((section (".intrinsics"))) __builtin_RNN_Get(void)                { return(0U); }                 ///< Gets the most recent pin state (either logic HIGH = 1 or LOW = 0)
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_Set(void)                { asm volatile ("nop \n"); }    ///< Sets the most recent pin state to logic HIGH
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_Clear(void)              { asm volatile ("nop \n"); }    ///< Sets the most recent pin state to logic LOW
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_Toggle(void)             { asm volatile ("nop \n"); }    ///< Toggles the most recent pin state
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetInput(void)           { asm volatile ("nop \n"); }    ///< Sets the pin direction to high impedance input (no state change)
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetOutput(void)          { asm volatile ("bclr _TRISA, #0 \n"); } ///< Sets the pin direction to low impedance output (no state change)
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetState(const bool s)   { asm volatile ("nop \n"); }    ///< Sets the pin direction to low impedance output (no state change)
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_Init(const bool d, const bool s) { asm volatile ("nop \n"); } ///< Initializes the pin setting direction (TRIS = d) and initial state (LAT = s)
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_Dispose(void)            { asm volatile ("nop \n"); }    ///< Sets the pin to high impedance input (RESET default)
+static inline bool __attribute__ ((section (".intrinsics"))) __builtin_RNN_GetOpenDrain(void)       { return(0U); }                 ///< Gets the pin open drain mode
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetOpenDrain(const bool a) { asm volatile ("nop \n"); }  ///< Sets the pin open drain mode
+static inline bool __attribute__ ((section (".intrinsics"))) __builtin_RNN_GetPullUp(void)          { return(0U); }                 ///< Gets the connection state of the pin's pull up resistor 
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetPullUp(const bool a)  { asm volatile ("nop \n"); }    ///< Sets the connection state of the pin's pull up resistor
+static inline bool __attribute__ ((section (".intrinsics"))) __builtin_RNN_GetPullDown(void)        { return(0U); }                 ///< Gets the connection state of the pin's pull down resistor
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetPullDown(const bool a) { asm volatile ("nop \n"); }   ///< Sets the connection state of the pin's pull down resistor
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetAnalog(void)          { asm volatile ("nop \n"); }    ///< Enable analog functions of this pin
+static inline void __attribute__ ((section (".intrinsics"))) __builtin_RNN_SetDigital(void)         { asm volatile ("nop \n"); }    ///< Disable analog functions of this pin
+
+
 // Function declarations for VSS connections
 static const P33C_GPIO_t PIN_NONE = {
     .Properties.Port        = -1,       ///< (not available)
@@ -54,6 +74,7 @@ static const P33C_GPIO_t PIN_NONE = {
     .Properties.PGA         = -1,       ///< (not available)
     .Properties.PG          = -1,       ///< (not available)
     .Properties.SPI         = -1,       ///< (not available)
+    
     .Objects.DAC            = NULL,     ///< (not available)
     .Objects.I2C            = NULL,     ///< (not available)
     #if (P33C_OPA_COUNT > 0)
@@ -64,23 +85,24 @@ static const P33C_GPIO_t PIN_NONE = {
     #endif
     .Objects.PG             = NULL,     ///< Pointer to PWM Generator peripheral instance data object available at this device pin
     .Objects.SPI            = NULL,     ///< Pointer to SPI interface peripheral instance data object available at this device pin
-    .Methods.Get            = NULL,     ///< Gets the most recent pin state (either logic HIGH = 1 or LOW = 0)
-    .Methods.Set            = NULL,     ///< Sets the most recent pin state to logic HIGH
-    .Methods.Clear          = NULL,     ///< Sets the most recent pin state to logic LOW
-    .Methods.Toggle         = NULL,     ///< Toggles the most recent pin state
-    .Methods.SetInput       = NULL,     ///< Sets the pin direction to high impedance input (no state change)
-    .Methods.SetOutput      = NULL,     ///< Sets the pin direction to low impedance output (no state change)
-    .Methods.SetState       = NULL,     ///< Sets the pin direction to low impedance output (no state change)
-    .Methods.Init           = NULL,     ///< Initializes the pin setting direction (TRIS = d) and initial state (LAT = s)
-    .Methods.Dispose        = NULL,     ///< Sets the pin to high impedance input (RESET default)
-    .Methods.GetOpenDrain   = NULL,     ///< Gets the pin open drain mode
-    .Methods.SetOpenDrain   = NULL,     ///< Sets the pin open drain mode
-    .Methods.GetPullUp      = NULL,     ///< Gets the connection state of the pin's pull up resistor 
-    .Methods.SetPullUp      = NULL,     ///< Sets the connection state of the pin's pull up resistor
-    .Methods.GetPullDown    = NULL,     ///< Gets the connection state of the pin's pull down resistor
-    .Methods.SetPullDown    = NULL,     ///< Sets the connection state of the pin's pull down resistor
-    .Methods.SetAnalog      = NULL,     ///< Enable analog functions of this pin
-    .Methods.SetDigital     = NULL      ///< Disable analog functions of this pin
+
+    .Methods.Get            = &__builtin_RNN_Get,           ///< Gets the most recent pin state (either logic HIGH = 1 or LOW = 0)
+    .Methods.Set            = &__builtin_RNN_Set,           ///< Sets the most recent pin state to logic HIGH
+    .Methods.Clear          = &__builtin_RNN_Clear,         ///< Sets the most recent pin state to logic LOW
+    .Methods.Toggle         = &__builtin_RNN_Toggle,        ///< Toggles the most recent pin state
+    .Methods.SetInput       = &__builtin_RNN_SetInput,      ///< Sets the pin direction to high impedance input (no state change)
+    .Methods.SetOutput      = &__builtin_RNN_SetOutput,     ///< Sets the pin direction to low impedance output (no state change)
+    .Methods.SetState       = &__builtin_RNN_SetState,      ///< Sets the pin direction to low impedance output (no state change)
+    .Methods.Init           = &__builtin_RNN_Init,          ///< Initializes the pin setting direction (TRIS = d) and initial state (LAT = s)
+    .Methods.Dispose        = &__builtin_RNN_Dispose,       ///< Sets the pin to high impedance input (RESET default)
+    .Methods.GetOpenDrain   = &__builtin_RNN_GetOpenDrain,  ///< Gets the pin open drain mode
+    .Methods.SetOpenDrain   = &__builtin_RNN_SetOpenDrain,  ///< Sets the pin open drain mode
+    .Methods.GetPullUp      = &__builtin_RNN_GetPullUp,     ///< Gets the connection state of the pin's pull up resistor 
+    .Methods.SetPullUp      = &__builtin_RNN_SetPullUp,     ///< Sets the connection state of the pin's pull up resistor
+    .Methods.GetPullDown    = &__builtin_RNN_GetPullDown,   ///< Gets the connection state of the pin's pull down resistor
+    .Methods.SetPullDown    = &__builtin_RNN_SetPullDown,   ///< Sets the connection state of the pin's pull down resistor
+    .Methods.SetAnalog      = &__builtin_RNN_SetAnalog,     ///< Enable analog functions of this pin
+    .Methods.SetDigital     = &__builtin_RNN_SetDigital     ///< Disable analog functions of this pin
 };
 
 #define PIN_VSS  PIN_NONE
